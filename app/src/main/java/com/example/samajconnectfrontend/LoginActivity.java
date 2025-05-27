0f2076d1139df5100d2ba89787dcaa6ef9394c7e
Hetvirani <hetvirani1305@gmail.com>
2025-05-27 16:56:18 +0530

    Hiiiiii

--- app/src/main/java/com/example/samajconnectfrontend/LoginActivity.java
+++ app/src/main/java/com/example/samajconnectfrontend/LoginActivity.java
@@ -38,7 +38,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-
+        System.out.println("hiiiiiiii");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
